ef0983f0e561db26e229c62b8edb0844aa8da2ea
Jason Chinsen <onzyone@gmail.com>
2017-11-08 19:44:01 -0500

    added 6.2.x and updated doco (#1)
    * added 6.2.x and updated doco
    
    * added 6.2.x and updated doco

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,5 +6,18 @@
 #
 # @example
 #   include cis
-class cis {
+#
+# @param apply_rhel7_level_one_scored [Boolean] defalut is false, to apply pass true to the cis module
+# @param apply_rhel7_level_one_notscored [Boolean] defalut is false, to apply pass true to the cis module
+# @param apply_rhel7_level_two_scored [Boolean] defalut is false, to apply pass true to the cis module
+# @param apply_rhel7_level_two_notscored [Boolean] defalut is false, to apply pass true to the cis module
+#
+
+class cis (
+  Boolean $apply_rhel7_level_one_scored,
+  Boolean $apply_rhel7_level_one_notscored,
+  Boolean $apply_rhel7_level_two_scored,
+  Boolean $apply_rhel7_level_two_notscored,
+){
+
 }
---
+++ manifests/redhat/rhel7/rule_6_2_19.pp
@@ -0,0 +1,10 @@
+# cis::redhat::rhel7::rule_6_2_19
+#
+# 6.2.19 Ensure no duplicate group names exist (Scored)
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include cis::redhat::rhel7::rule_6_2_19
+class cis::redhat::rhel7::rule_6_2_19 {
+}
